2765179204fc96e56be35965bba01199c3409b64
LucasMartins717 <martinsx787@gmail.com>
2024-11-08 15:04:27 -0300

    1.2

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const pool = require('./db');
 
 app.listen(3000, () => {
     console.log("server iniciado!")
 })
 
-app.get('/', (req, res) => 
-res.send('<h1 style="color: blue">Criando um servidor!</h1>')
+app.get('/', (req, res) =>
+    res.send('<h1 style="color: blue">Criando um servidor!</h1>')
 )
+
+app.get('/users', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (error) {
+        console.log("Erro: " + error);
+        res.status(500).send('Erro ao buscar usuarios!')
+    }
+})
